88fb004a83f5360d15bd8a0078a46a032592c310
billpahutski <billpahutski@gmail.com>
2026-03-23 20:08:31 -0700

    pulling memes now

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-const images = [
-  "https://picsum.photos/300?random=1",
-  "https://picsum.photos/300?random=2",
-  "https://picsum.photos/300?random=3"
-];
-
-function showRandomImage() {
-  const randomIndex = Math.floor(Math.random() * images.length);
-  document.getElementById("randomImage").src = images[randomIndex];
-}
-
 window.addEventListener("load", function() {
-  const button = document.getElementById("randomBtn");
-  button.addEventListener("click", showRandomImage);
-  showRandomImage();
+  const firstMeme = memes[0];
+  const image = document.getElementById("randomImage");
+  image.src = firstMeme.image;
 });
